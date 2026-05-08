e096b9a30d045f9184a6f2881a4464457b18d743
VINOTH KUMAR C <cvinothkumar.1995@gmail.com>
2026-05-08 16:54:45 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,20 +1,12 @@
 package com.example;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@SpringBootApplication
-@RestController
 public class App {
 
-    public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+    public int add(int a, int b) {
+        return a + b;
     }
 
-    @GetMapping("/")
-    public String home() {
-        return "Hello from Jenkins Docker Pipeline";
+    public static void main(String[] args) {
+        System.out.println("Hello Docker + Jenkins!");
     }
 }
